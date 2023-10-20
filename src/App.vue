f8cd63031b24dd4ae5a16a4a5982d0c0e4c8ce55
Dmitry Sushkov <sushkovd98@mail.ru>
2023-10-20 15:55:39 +0200

    Add: Sidebar layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="app">
     <Header :title="'Жилфонд'" />
+    <div class="main">
+      <Sidebar />
+    </div>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Sidebar from './components/Sidebar.vue';
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Sidebar,
   }
 }
 </script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sidebar">
+    <h2>Поиск сотрудников</h2>
+
+    <input placeholder="Введите Id или имя">
+
+    <p class="result-header">Результаты</p>
+    <div class="result">
+      <!-- <p>начните поиск </p> -->
+      <div class="cards">
+        <div class="card">
+          <div class="img">
+            <img src="../assets/415x415_1369476198592009862_1369477640844005775.jpg" alt="">
+          </div>
+          <div class="card-info">
+            <p class="name">Bret</p>
+            <p class="email">Sincere@april.biz</p>
+          </div>
+        </div>
+        <div class="card active">
+          <div class="img">
+            <img src="../assets/415x415_1369476198592009862_1369477640844005775.jpg" alt="">
+          </div>
+          <div class="card-info">
+            <p class="name">Bret</p>
+            <p class="email">Sincere@april.biz</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Sidebar'
+  }
+</script>
+
+<style src="../style/sidebar.scss" scoped lang="scss"></style>
