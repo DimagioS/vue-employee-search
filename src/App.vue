b97a6975b7eb70f4d46d018a13d7de945b0f8862
Dmitry Sushkov <sushkovd98@mail.ru>
2023-10-20 12:24:58 +0200

    Add: Header component

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <Header :title="'Жилфонд'" />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style src="./style/app.scss" lang="scss"></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <div class="logo-container">
+      <h1 class="logo">{{ title }}</h1>
+    </div>
+    <div class="user-container">
+      <p class="user">Пользователь</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    props: {
+      title: {
+        type: String,
+        required: true,
+      }
+    }
+  }
+</script>
+
+<style src="../style/header.scss" scoped lang="scss"></style>
