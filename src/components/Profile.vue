83975f6170be65dcb3e071b0b75ebc03d1343cd8
Dmitry Sushkov <sushkovd98@mail.ru>
2023-10-23 22:25:42 +0200

    Add: Profile layout

---
+++ src/components/Profile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="profile">
+    <div class="profile-img">
+      <img src="../assets/profile-photo.png" alt="">
+    </div>
+    <div class="profile-info">
+      <h3 class="profile-name">Ervin Howell</h3>
+      <div class="profile-contact">
+        <p>Email:  <span>Shanna@melissa.tv</span></p>
+        <p>Phone:  <span>010-692-6593 x09125</span></p>
+      </div>
+      <div class="profile-about-me">
+        <span>О себе:</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Profile',
+  }
+</script>
+
+<style src="../style/profile.scss" scoped lang="scss"></style>
