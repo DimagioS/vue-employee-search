dfca08d184ee6e6eccdf7046b961229891a7dffc
Dmitry Sushkov <sushkovd98@mail.ru>
2023-10-23 11:13:31 +0200

    Refactor: Update caching structure for users

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,7 @@ export default createStore({
     return {
       searchQuery: '',
       users: [],
-      cachedUsers: {},
+      cachedUsersBuId: new Map(),
       isLoading: false,
     }
   },
@@ -28,14 +28,13 @@ export default createStore({
       state.users = [];
     },
 
-    SET_CACHED_USERS(state, payload) {
-      state.cachedUsers[payload.id] = payload.user;
+    SET_CACHED_USERS_BY_ID(state, payload) {
+      state.cachedUsersBuId.set(payload.id, payload.user) 
     },
 
     REMOVE_OLDEST_CACHED_USER(state) {
-      const keys = Object.keys(state.cachedUsers);
-      const firstKey = keys[0];
-      delete state.cachedUsers[firstKey];
+      const firstKey = state.cachedUsersBuId.keys().next().value;
+      state.cachedUsersBuId.delete(firstKey);
     },
 
     SET_LOADING(state, loading) {
@@ -58,23 +57,22 @@ export default createStore({
 
       if (regex.test(state.searchQuery)) {
         commit('RESET_USERS');
-        let idsToFetch = [];
-        let cachedUserIds = Object.keys(state.cachedUsers);
+        let queriesToFetch = [];
         const querysArray = [...new Set(state.searchQuery.split(',').map((user) => user.trim()))];
 
         querysArray.forEach((query) => {
-          if (cachedUserIds.indexOf(query) === -1 && query !== '') {
-            idsToFetch.push(query);
-          } else {
-            commit('SET_USER', state.cachedUsers[query]);
+          if (state.cachedUsersBuId.has(parseInt(query))) {
+            commit('SET_USER', state.cachedUsersBuId.get(parseInt(query)));
             return; 
+          } else {
+            queriesToFetch.push(query);
           }
         });
 
         commit('SET_LOADING', true);
 
         try {
-          const promises = idsToFetch.map((user) => {
+          const promises = queriesToFetch.map((user) => {
             const apiType = isNaN(user) 
               ? `${API_URL_USERNAME}=${user}` 
               : `${API_URL_ID}=${user}`;
@@ -88,15 +86,15 @@ export default createStore({
             const user = response.data?.[0];
 
             if (user !== undefined) {
-              if (cachedUserIds.length >= MAX_CACHE_SIZE) {
+              if (state.cachedUsersBuId.size >= MAX_CACHE_SIZE) {
                 commit('REMOVE_OLDEST_CACHED_USER');
               }
 
-              commit('SET_CACHED_USERS', { user, id: user.id });
+              commit('SET_CACHED_USERS_BY_ID', { user: user, id: user.id });
               commit('SET_USER', user);
             }
 
-            idsToFetch = [];
+            queriesToFetch = [];
           })
         } catch (error) {
           console.error('Произошла ошибка при получении данных:', error);
